e244eeac0f18164134d6e7f52682be5f285c36dc
unknown <borage.suraj113@gmail.com>
2021-09-02 20:10:29 +0530

    [ suraj] UC 1 EMP ATTENDANCE

---
+++ EmployeeWageCompute058/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EmployeeWageCompute058
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constants
+            int IS_PRESENT = 1;
+
+            Random random = new Random();
+
+            int IS_ABSENT = random.Next(0 , 2);
+
+            //Selection statement
+            if 
+                (1 == IS_PRESENT)
+                Console.WriteLine("EMPLOYEE IS PRESENT");
+            else
+                Console.WriteLine("EMPLOYEE IS ABSENT");
+            Console.Read();
+
+
+        }
+    }
+}
