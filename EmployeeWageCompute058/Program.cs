3087d9efe8ed7466c940f36bb6cd68a24d1b2e9d
unknown <borage.suraj113@gmail.com>
2021-09-03 08:58:16 +0530

    [suraj] UCTHREE

--- EmployeeWageCompute058/Program.cs
+++ EmployeeWageCompute058/Program.cs
@@ -5,20 +5,33 @@ namespace EmployeeWageCompute058
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //UC2
             //constants
-            int IS_PRESENT = 1;
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HR = 20;
+            int empHrs = 0;
+            int empWage = 0;
 
+            // per defind random
             Random random = new Random();
 
-            int IS_ABSENT = random.Next(0 , 2);
+            //next method () 0 is initial valu ,2 number of element from 0
+            int empCheck = random.Next(0, 2);
 
-            //Selection statement
-            if 
-                (1 == IS_PRESENT)
+            // selection staments
+            if (empCheck == IS_FULL_TIME)
+            {
                 Console.WriteLine("EMPLOYEE IS PRESENT");
+                empHrs = 8;
+            }
             else
+            {
                 Console.WriteLine("EMPLOYEE IS ABSENT");
+                empHrs = 0;
+            }
+
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Daily Employee Wage is =" + empWage);
             Console.Read();
 
 
